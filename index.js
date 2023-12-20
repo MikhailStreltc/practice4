be050c65ac0eb624e85680c0c4852d7e134f4636
MikhailStreltc <mstreltc@gmail.com>
2023-12-20 16:32:55 +0300

    salary git commit

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-consolesss dsfsdf
+function calculate_salary(role) {
+    let salary = 0;
+    if (role === 'engineer') {
+        salary = 4000;
+    } else{
+        salary = 1500;
+    }
+    return salary;
+}
+
+let worker1 = {
+    name: 'Vasya',
+    role: 'worker',
+    age: 23,
+};
+
+let engineer1 = {
+    name: 'Grigory',
+    role: 'engineer',
+    age: 42,
+};
+
+let worker2 = {
+    name: 'Grisha',
+    role: 'worker',
+    age: 25,
+};
+
+let engineer2 = {
+    name: 'San Sanich',
+    role: 'engineer',
+    age: 54,
+}
+
+let empliyees = [worker1, engineer1, worker2];
+empliyees.push(engineer2)
+
+let total_salary = 0;
+empliyees.forEach(function (employee) {
+    let salary = calculate_salary(employee.role);
+    total_salary += salary;
+})
+
+console.log(total_salary);
